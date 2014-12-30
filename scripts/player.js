5cc8231d67ae9fefdee5f6c6fd0c0cb6aa46fa26
Geoffrey P <geoffreyplichard@gmail.com>
2014-12-30 10:47:18 +0100

    Inject player dependencies & import image

--- scripts/player.js
+++ scripts/player.js
@@ -1,18 +1,19 @@
 /*
 * ['file name'] (returned object name)
 */
-define([], function(){
+define(['graphics', 'canvas'], function(graphics, canvas){
 	'use strict';
 	
 	function Player(x, y, width, height){
 		this.x = x;
-		thix.y = y;
+		this.y = y;
 		this.width = width;
 		this.heght = height;
+		this.img = null;
 	}
 	
 	Player.prototype.init = function(){
-		
+		this.img = graphics.get('player_ship');
 	};
 	
 	Player.prototype.update = function(){
@@ -20,7 +21,7 @@ define([], function(){
 	};
 	
 	Player.prototype.render = function(){
-		
+		canvas.context.drawImage(this.img, this.x, this.y);
 	};
 	
 	return Player;
