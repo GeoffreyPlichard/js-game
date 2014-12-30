f1292aa945d47666a2bdbf8274f65a98eae51103
Geoffrey P <geoffreyplichard@gmail.com>
2014-12-30 10:42:27 +0100

    Creation of player object

---
+++ scripts/player.js
@@ -0,0 +1,28 @@
+/*
+* ['file name'] (returned object name)
+*/
+define([], function(){
+	'use strict';
+	
+	function Player(x, y, width, height){
+		this.x = x;
+		thix.y = y;
+		this.width = width;
+		this.heght = height;
+	}
+	
+	Player.prototype.init = function(){
+		
+	};
+	
+	Player.prototype.update = function(){
+		
+	};
+	
+	Player.prototype.render = function(){
+		
+	};
+	
+	return Player;
+	
+});
